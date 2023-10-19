8944695c47f20cfc6beb33a36e6e3b804088c7bb
joshuaeh <jhammond02@gmail.com>
2023-10-19 09:53:53 -0500

    jitterfy_categorical_scatterplot function

--- jtoolbox/graph.py
+++ jtoolbox/graph.py
@@ -30,6 +30,7 @@ class TickRedrawer(matplotlib.artist.Artist):
                         artist.draw(renderer)
 
         renderer.close_group(self.__name__)
+        self.stale = False
         
 def append_to_legend(ax, new_legend_entries):
     """ """
@@ -48,4 +49,25 @@ def wrap_labels(ax, width, break_long_words=False):
         labels.append(textwrap.fill(text, width=width,
                       break_long_words=break_long_words))
     ax.set_xticklabels(labels, rotation=0)
+    return
+
+def jitterfy_categorical_scatterplot(ax, aggregation_statistic=None, jitter_width=0.6):
+    """Jitterfy a categorical scatterplot"""
+    for points in ax.collections:
+        vertices = points.get_offsets().data
+        if aggregation_statistic is not None:
+            group_x_coordinates = np.unique(vertices[:,0])
+            for group_x_coordinate in group_x_coordinates:
+                group_vertices = vertices[vertices[:, 0] == group_x_coordinate]
+                group_aggregate = aggregation_statistic(group_vertices[:, 1])
+                ax.hlines(group_aggregate,
+                    group_x_coordinate - jitter_width/2,
+                    group_x_coordinate + jitter_width/2,
+                    color="black", alpha=0.9, linewidth=1,
+                    linestyle="--")
+        if len(vertices) > 0:
+            vertices[:, 0] += np.random.uniform(-jitter_width/2, jitter_width/2, vertices.shape[0])
+            points.set_offsets(vertices)
+    xticks = ax.get_xticks()
+    ax.set_xlim(xticks[0] - jitter_width, xticks[-1] + jitter_width) # the limits need to be moved to show all the jittered dots
     return
